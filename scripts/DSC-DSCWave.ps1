3d836d2bff260841ce1d7e9ec85d1101ad8138fd
safiur <safiuremailid@gmail.com>
2019-05-24 18:28:02 +0530

    Update DSC-DSCWave.ps1

--- scripts/DSC-DSCWave.ps1
+++ scripts/DSC-DSCWave.ps1
@@ -1,20 +1,19 @@
-configuration DSCWave {
-    Archive DSCWave {
-        DependsOn = '[Script]DSCWave';
-        Ensure = 'Present';
-        Path = "$env:windir\temp\DSC Resource Kit Wave 6 08212014.zip";
-        Destination = "$env:ProgramFiles\WindowsPowerShell\Modules";
-    }
+Archive WebServerConfig {
+	DependsOn = '[Script]WebServerConfig';
+	Ensure = 'Present';
+	Path = "$env:windir\temp\DSC Resource Kit Wave 10 04012015.zip";
+	Destination = "$env:ProgramFiles\WindowsPowerShell\Modules";
+}
 
-    Script DSCWave {
-        GetScript = { @{ Result = (Test-Path -Path "$env:windir\temp\DSC Resource Kit Wave 6 08212014.zip"); } };
-        SetScript = {
-            $Uri = 'http://gallery.technet.microsoft.com/DSC-Resource-Kit-All-c449312d/file/124120/1/DSC%20Resource%20Kit%20Wave%206%2008212014.zip';
-            $OutFile = "$env:windir\temp\DSC Resource Kit Wave 6 08212014.zip";
-            Invoke-WebRequest -Uri $Uri -OutFile $OutFile;
-            Unblock-File -Path $OutFile;
-            };
-        TestScript = { Test-Path -Path "$env:windir\temp\DSC Resource Kit Wave 6 08212014.zip"; }
-    }
+  Script WebServerConfig {
+	GetScript = { @{ Result = (Test-Path -Path "$env:windir\temp\DSC Resource Kit Wave 10 04012015.zip"); } };
+	SetScript = {
+		$Uri = 'https://gallery.technet.microsoft.com/scriptcenter/DSC-Resource-Kit-All-c449312d/file/131371/4/DSC%20Resource%20Kit%20Wave%2010%2004012015.zip';
+		$OutFile = "$env:windir\temp\DSC Resource Kit Wave 10 04012015.zip";
+		Invoke-WebRequest -Uri $Uri -OutFile $OutFile;
+		Unblock-File -Path $OutFile;
+		};
+	TestScript = { Test-Path -Path "$env:windir\temp\DSC Resource Kit Wave 10 04012015.zip"; } 
+ }
 }
 . .\WebServerConfig.ps1
