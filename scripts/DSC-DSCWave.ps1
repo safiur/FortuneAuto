51159ebb24ee494f0adc1bf9a08641a887d96b53
safiur <safiuremailid@gmail.com>
2019-05-25 13:44:37 +0530

    Update DSC-DSCWave.ps1

--- scripts/DSC-DSCWave.ps1
+++ scripts/DSC-DSCWave.ps1
@@ -1,11 +1,12 @@
-Archive WebServerConfig {
+Configuration DSCWave {
+Archive DSCWave {
 	DependsOn = '[Script]WebServerConfig';
 	Ensure = 'Present';
 	Path = "$env:windir\temp\DSC Resource Kit Wave 10 04012015.zip";
 	Destination = "$env:ProgramFiles\WindowsPowerShell\Modules";
 }
 
-  Script WebServerConfig {
+  Script DSCWave {
 	GetScript = { @{ Result = (Test-Path -Path "$env:windir\temp\DSC Resource Kit Wave 10 04012015.zip"); } };
 	SetScript = {
 		$Uri = 'https://gallery.technet.microsoft.com/scriptcenter/DSC-Resource-Kit-All-c449312d/file/131371/4/DSC%20Resource%20Kit%20Wave%2010%2004012015.zip';
@@ -16,4 +17,4 @@ Archive WebServerConfig {
 	TestScript = { Test-Path -Path "$env:windir\temp\DSC Resource Kit Wave 10 04012015.zip"; } 
  }
 }
-. .\WebServerConfig.ps1
+. ./WebServerConfig.ps1
