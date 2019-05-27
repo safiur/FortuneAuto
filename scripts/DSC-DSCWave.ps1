e15852afc9bdeeb9eb472356849a19d622bc34da
safiur <safiuremailid@gmail.com>
2019-05-27 12:59:52 +0530

    Update DSC-DSCWave.ps1

--- scripts/DSC-DSCWave.ps1
+++ scripts/DSC-DSCWave.ps1
@@ -1,19 +1,11 @@
-Configuration DSCWave {
-Archive DSCWave {
-	DependsOn = '[Script]DSCwave';
-	Ensure = 'Present';
-	Path = "$env:windir\temp\DSC Resource Kit Wave 10 04012015.zip";
-	Destination = "$env:ProgramFiles\WindowsPowerShell\Modules";
-}
+$clnt = new-object System.Net.WebClient
+$url = "https://gallery.technet.microsoft.com/scriptcenter/DSC-Resource-Kit-All-c449312d/file/131371/4/DSC%20Resource%20Kit%20Wave%2010%2004012015.zip"
+$file = "C:\Windows\Temp\DSC Resource Kit Wave 10 04012015.zip"
+$clnt.DownloadFile($url,$file)
 
-  Script DSCWave {
-	GetScript = { @{ Result = (Test-Path -Path "$env:windir\temp\DSC Resource Kit Wave 10 04012015.zip"); } };
-	SetScript = {
-		$Uri = 'https://gallery.technet.microsoft.com/scriptcenter/DSC-Resource-Kit-All-c449312d/file/131371/4/DSC%20Resource%20Kit%20Wave%2010%2004012015.zip';
-		$OutFile = "$env:windir\temp\DSC Resource Kit Wave 10 04012015.zip";
-		Invoke-WebRequest -Uri $Uri -OutFile $OutFile;
-		Unblock-File -Path $OutFile;
-		};
-	TestScript = { Test-Path -Path "$env:windir\temp\DSC Resource Kit Wave 10 04012015.zip"; } 
- }
-}
+$shell_app=new-object -com shell.application 
+$zip_file = $shell_app.namespace($file) 
+$destination = $shell_app.namespace("C:\Program Files\WindowsPowerShell\Modules") 
+Remove-Item "C:\Program Files\WindowsPowerShell\Modules*.*"  -recurse -force -ea silentlycontinue
+$destination.Copyhere($zip_file.items())
+
